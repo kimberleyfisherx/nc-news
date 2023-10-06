5be70c0d63f1351bac463ea769633598862f81b7
Kimberley <kimberley@Heathers-MacBook-Air.local>
2023-10-06 09:13:25 +0100

    removing commented out code

--- api/app.js
+++ api/app.js
@@ -10,25 +10,15 @@ const {
   deleteCommentById,
   fetchUsers,
   getArticles,
-} = require("./controllers/controller.api"); //imports fetch function from controllers
+} = require("./controllers/controller.api");
 
-const app = express(); // initialise express (object to handle middleware and http)
-app.use(express.json()); // app.use handles specific requests - here we are parse that data and making it available in the req.body to use later
+const app = express();
+app.use(express.json());
 
-app.get("/api/topics", fetchTopics); // defines a GET route, When a GET request made ,call the fetchTopics function made in controller file
-
+app.get("/api/topics", fetchTopics);
 app.get("/api/", fetchApi);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", fetchArticleId);
-
-/*app.get("/api/articles", (req, res) => {
-  var topic = req.query;
-  if (topic != null) {
-    getArticles(topic.topic);
-  } else {
-    fetchArticles();
-  }
-});*/
 
 app.get("/api/articles/:article_id/comments", fetchCommentsById);
 
@@ -42,8 +32,7 @@ app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Request not found" });
-}); // error handling, if request is made that doesnt have a path, returns 404 code and msg
-
+});
 app.use((err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ msg: err.msg });
